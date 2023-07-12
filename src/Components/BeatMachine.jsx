2ae0690bdf3185079a8e9a893edb4bcc20244245
AbhayNumb <bhanusultan@gmail.com>
2023-07-12 14:46:04 +0530

    add all

---
+++ src/Components/BeatMachine.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../styles/index.css';
+import './PlayButton'
+import PlayButton from './PlayButton';
+import '../styles/beatMachine.css';
+
+
+const BeatMachine = () => {
+
+    return(
+      <>
+        <div className="beatMachineTitle"><span className="beat">DJWale </span><span className="juice">Babu</span></div>
+        {/* <PlayButton /> */}
+      </>
+    )
+  }
+
+export default BeatMachine;
